d47997ac16b575c7cd3dc2f430926c821088ef31
Your Name <you@example.com>
2026-09-11 15:00:27 +0200

    mer stoff

--- startfile.py
+++ startfile.py
@@ -3,7 +3,13 @@ from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
 # Raketens bana kan beräknas med Newtons rörelselagar i följande differentialekvation
-# m(t)¯a(t) = ¯F + m′(t)¯u(t)
+
+# m(t)a(t) = F + m'(t)u(t)
+
+# F = m(t)g − 0.05||v(t)||v(t)
+
+# u(t)= (ux(t), uy(t)) = (700*cos(-pi/2), 700*sin(-pi/2))
+
 
 def ode_Y(t, y):
     yder=np.zeros(1)
@@ -15,9 +21,12 @@ v0 = [0]
 
 m0 = [8, -0.4]
 
-t_eval= []
+y0 = [(0,0)]
 
-sol = solve_ivp(ode_Y, )
+t_eval= [0, 10, 200]
+tspan = [0, 10]
+
+sol = solve_ivp(ode_Y, tspan, y0,  t_eval=t_eval)
 
 plt.plot(sol.t, sol.y[0])
 plt.xlabel("t")
