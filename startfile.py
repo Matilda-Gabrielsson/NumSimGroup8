b5da4fa1ed4164afb340270ab195fcf48ae0c9eb
Matilda-Gabrielsson <matil@Matilda-Dator.localdomain>
2026-09-16 14:06:58 +0200

    jahopp

--- startfile.py
+++ startfile.py
@@ -2,14 +2,31 @@ import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
-# hejhej
 
-# funkar det nu
 def theta(t):
     return 1
+
+def mass(t):
+    if t <= 10:
+        mass = 8 - 0.4 * t
+    else:
+        mass = 4
+    return mass
+
 
 def raketbana(t, y):
     return 0
 
+km = 700
+
+u = np.array([
+    km * np.cos(theta(t)),
+    km * np.sin(theta(t))
+])
 
 tspan = [0, 10]
+
+target_x = 80
+target_y = 60
+plt.plot(target_x, target_y, 'o')
+plt.show()
---
+++ test.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+y = (, 0.4)
+plt.plot(y, 'o')
+plt.show()
